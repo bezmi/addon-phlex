6ed86ba12d647d57da210d1dc95af19b6e8fa8d2
shebyimran@gmail.com <shebyimran@gmail.com>
2017-11-24 04:52:22 +1000

    fixed install script

--- addon-phlex/rootfs/etc/cont-init.d/10-install.sh
+++ addon-phlex/rootfs/etc/cont-init.d/10-install.sh
@@ -3,16 +3,19 @@
 # spellcheck disable=SC1091
 
 source /usr/lib/hassio-addons/base.sh
+mkdir /var/www/html/Phlex/
 
 #fetch repository
-if [! -d /var/www/Phlex/.git ]; then
+if [ ! -d /var/www/html/Phlex/.git ]; then
     echo "No repo found, pulling."
-        git clone https://github.com/d8ahazard/Phlex /var/www/html/Phlex
+    cd /var/www/html/Phlex/
+    git clone https://github.com/d8ahazard/Phlex .
+    cd -
 fi
 
 echo "Updating Phlex."
-cd /var/www/html/Phlex
+cd /var/www/html/Phlex/
 git pull
 
 #Permissions
-chown -R nginx /var/www/html/
+chown -R nginx /var/www/html/Phlex/
