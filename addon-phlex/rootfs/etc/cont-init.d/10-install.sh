65ddbc7b3de1f2331824be5ab7be52a5d9c8e59d
shebyimran@gmail.com <shebyimran@gmail.com>
2017-11-24 08:09:33 +1000

    changed main location to /data/

--- addon-phlex/rootfs/etc/cont-init.d/10-install.sh
+++ addon-phlex/rootfs/etc/cont-init.d/10-install.sh
@@ -3,19 +3,19 @@
 # spellcheck disable=SC1091
 
 #source /usr/lib/hassio-addons/base.sh
-mkdir /var/www/html/Phlex/
+mkdir -p /data/www/html/Phlex/
 
 #fetch repository
-if [ ! -d /var/www/html/Phlex/.git ]; then
+if [ ! -d /data/www/html/Phlex/.git ]; then
     echo "No repo found, pulling."
-    cd /var/www/html/Phlex/
+    cd /data/www/html/Phlex/
     git clone https://github.com/d8ahazard/Phlex .
     cd -
 fi
 
 echo "Updating Phlex."
-cd /var/www/html/Phlex/
+cd /data/www/html/Phlex/
 git pull
 
 #Permissions
-chown -R nginx /var/www/html/Phlex/
+chown -R nginx /data/www/html/Phlex/
