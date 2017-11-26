d505132d43ebdc13417b560d7bd4d899bec1ef9e
shebyimran@gmail.com <shebyimran@gmail.com>
2017-11-27 07:55:16 +1000

    added apache version

--- addon-phlex/rootfs/etc/cont-init.d/10-install.sh
+++ addon-phlex/rootfs/etc/cont-init.d/10-install.sh
@@ -5,17 +5,18 @@
 #source /usr/lib/hassio-addons/base.sh
 
 #fetch repository
-if [ ! -d /data/www/html/Phlex/.git ]; then
+if [ ! -d /data/www/localhost/htdocs/Phlex/.git ]; then
     echo "No repo found, pulling."
-    mkdir -p /data/www/html/Phlex/
-    cd /data/www/html/Phlex/
+    mkdir -p /data/www/localhost/htdocs/Phlex/
+    cd /data/www/localhost/htdocs/Phlex/
     git clone https://github.com/d8ahazard/Phlex .
     cd -
 fi
 
 echo "Updating Phlex."
-cd /data/www/html/Phlex/
+cd /data/www/localhost/htdocs/Phlex/
 git pull
+cd -
 
 if [ ! -f /share/config.ini.php ]; then
     echo "config file not found, creating in /share"
@@ -24,5 +25,5 @@ fi
 
 #Permissions
 ln -s /share/config.ini.php /data/www/html/Phlex/config.ini.php
-chown nginx /share/config.ini.php
-chown -R nginx /data/www/html/Phlex/
+chown apache /share/config.ini.php
+chown -R apache /data/www/localhost/htdocs/Phlex/
