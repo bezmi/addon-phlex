5282915679892a965e1a2dad1eeffcc22c829790
shebyimran@gmail.com <shebyimran@gmail.com>
2017-11-27 07:47:43 +1000

    added apache version

---
+++ 2addon-phlex/addon-phlex/rootfs/etc/cont-init.d/11-apache.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bash
+
+# shellcheck disable=SC1091
+
+source /usr/lib/hassio-addons/base.sh
+
+declare port
+declare fastcgiport
+#declare certfile
+#declare keyfile
+
+port = $(hass.config.get 'port')
+fastcgiport = $(hass.config.get 'fastcgiport')
+
+sed -i "s/%%port%%/${port}/g" /etc/apache2/httpd.conf
+sed -i "s/%%fastcgiport%%/${fastcgiport}/g" /etc/apache2/httpd.conf
+sed -i "s/%%fastcgiport%%/${fastcgiport}/g" /etc/php7/php-fpm.d/www.conf
+sed -i "s/%%port%%/${port}/g" /etc/php7/php-fpm.d/www.conf
+
---
+++ 2addon-phlex/addon-phlex/rootfs/etc/cont-init.d/13-nginx.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: Pi-hole
+# Configures NGINX for use with Pi-hole
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+declare certfile
+declare keyfile
+declare port
+declare fastcgiport
+
+port=$(hass.config.get 'port')
+fastcgiport=$(hass.config.get 'fastcgiport')
+
+if hass.config.true 'ssl'; then
+    rm /etc/nginx/nginx.conf
+    mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
+
+    certfile=$(hass.config.get 'certfile')
+    keyfile=$(hass.config.get 'keyfile')
+
+    sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
+    sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
+fi
+
+sed -i "s/%%port%%/${port}/g" /etc/nginx/nginx.conf
+sed -i "s/%%fastcgiport%%/${fastcgiport}/g" /etc/nginx/nginx.conf
+sed -i "s/%%fastcgiport%%/${fastcgiport}/g" /etc/php7/php-fpm.d/www.conf
